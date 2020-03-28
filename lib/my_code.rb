eafd5528a9b78a4f6832a9086335c1193cf15e15
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 02:08:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,5 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length do
-    if source
+ if source_array.all?
 end
