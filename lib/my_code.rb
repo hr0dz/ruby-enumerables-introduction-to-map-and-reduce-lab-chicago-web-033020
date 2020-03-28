053f9d163ea2052fa5b27c2e94793eaa62f3f56e
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 01:53:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,6 +39,7 @@ end
 def reduce_to_total(source_array, starting_point)
   i = 0
   x = starting_point.any?(Integer)
+  if x
   total = starting_point
   while i < source_array.length do
     total += source_array[i]
